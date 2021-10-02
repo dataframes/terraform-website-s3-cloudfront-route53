2db233f49ce7fc02ba18d49f05b8b5e159e84568
Anton Podviaznikov <anton@podviaznikov.com>
2021-10-02 09:27:48 -0700

    Update main.tf

--- site-main/main.tf
+++ site-main/main.tf
@@ -50,6 +50,14 @@ resource "aws_s3_bucket" "website_bucket" {
   versioning {
     enabled = var.versioning
   }
+  
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_origins = [var.domain]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
 
   //  logging {
   //    target_bucket = "${var.log_bucket}"
